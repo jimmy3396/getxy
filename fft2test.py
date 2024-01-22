918aba15525ca88e83228c26a0af630ce8c5f82b
jimmy_lee <jimmy3396@seoul.ac.kr>
2024-01-22 13:08:04 +0900

    before add two file version

--- fft2test.py
+++ fft2test.py
@@ -5,6 +5,7 @@ Created on Fri Jan 19 19:45:18 2024
 @author: 82109
 """
 
+
 from diffractio import um, mm, degrees, np, plt
 from diffractio.scalar_sources_XY import Scalar_source_XY
 from diffractio.scalar_masks_XY import Scalar_mask_XY
@@ -20,20 +21,22 @@ yout = np.linspace(-10*mm, 10*mm, 1024)
 # using gaussian beam for light source
 u0 = Scalar_source_XY(x=xin, y=yin, wavelength=wavelength)
 u0.gauss_beam(A=1, r0=(0*um,0*um), z0=0, w0=(200*um, 200*um), phi=0*degrees, theta=0*degrees)
-
+u0.draw(filename='usage1.png')
 
 # lens, used to shift focus in z direction
 t0 = Scalar_mask_XY(x=xin, y=yin, wavelength=wavelength)
 t0.lens(r0=(0*um, 0*um), radius=(2000*um, 2000*um), focal=z, angle=0*degrees)
-
+t0.draw(filename='usage2.png')
 
 # linear phase mask, used to shift focus in xy plane
 t1 = Scalar_mask_XY(x=xin, y=yin, wavelength=wavelength)
 t1.blazed_grating(period=25*um, phase_max=2*np.pi, x0=0, angle=100*degrees)
 
 t2=t0*t1
+t2.draw(filename='usage3.png')
 
 u1 = t2*u0
 #u2 = u1.fft(remove0 = False, new_field = True)
 u2 = u1.CZT(z, xout, yout)
-u2.draw(kind = 'intensity', logarithm=0.1) # plot intensity as logplot
+u2.draw(filename='usage4.png',kind = 'intensity', logarithm=0.1) # plot intensity as logplot
+
